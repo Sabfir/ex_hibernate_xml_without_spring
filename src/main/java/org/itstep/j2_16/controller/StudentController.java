033cf39e2416668a08ee14135c6151f66b2c534b
Andrenalin1488 <andrew.zakharow14@gmail.com>
2017-09-03 20:14:33 +0300

    azakharow/spring_mvc_pure_java_tomkat

--- src/main/java/org/itstep/j2_16/controller/StudentController.java
+++ src/main/java/org/itstep/j2_16/controller/StudentController.java
@@ -1,13 +1,13 @@
 package org.itstep.j2_16.controller;
 
 
-        import org.itstep.j2_16.entity.Student;
-        import org.itstep.j2_16.service.StudentService;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.web.bind.annotation.*;
+import org.itstep.j2_16.entity.Student;
+import org.itstep.j2_16.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-        import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/students")
